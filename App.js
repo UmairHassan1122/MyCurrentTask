741d2e968249c0e8e181db6c7fd80a02ff002915
Abdul Rehman <abdulrehmanarain063@gmail.com>
2021-11-22 17:11:18 +0500

    Done

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ fontFamily: "ComforterBrush-Regular" }}> Hello This is Gaszik App </Text>
+      </View>
+    )
+  }
+}
